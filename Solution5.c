7b57549d217a3563797b8b7d1a0b290872e198a8
socketgut <71415224+varadpal@users.noreply.github.com>
2024-02-10 10:10:26 +0530

    Add files via upload

---
+++ Solution5.c
@@ -0,0 +1,41 @@
+// 5. Program to count vowels and consonants in a string using pointer. 
+
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char const *argv[]){
+    char vowels[10] = "AEIOUaeiou";
+    char word[50] = {"\0"}; 
+
+    int vowels_count = 0, consonents_count = 0; 
+    printf("Enter Word: "); 
+    scanf("%s", &word);
+
+    char *ptrw; 
+    ptrw = word; 
+    // printf("%s\n", word);
+    
+    for (int i = 0; i < strlen(word); i++)
+    {
+        for (int j = 0; j < strlen(vowels); j++)
+        {
+            if (*(&ptrw[i]) == vowels[j])
+        {
+            // printf("%d Found\n", +1);
+            vowels_count++; 
+        }
+        else
+        {
+            continue;
+        }
+        
+        }   
+    }
+
+    consonents_count = strlen(word) - vowels_count; 
+
+    printf("No. of Vowels = %d\n", vowels_count);
+    printf("No. of Consonents = %d\n", consonents_count);
+
+    // printf("%d", strchr(word, 'A')); 
+    return 0;
+}
