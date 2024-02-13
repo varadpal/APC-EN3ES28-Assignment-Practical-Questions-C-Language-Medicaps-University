30e164414ce569eacb121901dd4c9d2fd9ea5282
socketgut <71415224+varadpal@users.noreply.github.com>
2024-02-13 09:15:55 +0530

    Create Soluution8.c

---
+++ Soluution8.c
@@ -0,0 +1,28 @@
+    #include <stdio.h> 
+
+    void Swap(int *a, int *b){
+        *a += *b; 
+        *b = *a - *b; 
+        *a -= *b; 
+    }
+
+    int main() {
+        // Defining Varibles
+        int num1, num2;
+
+        // Taking Valkues from User
+        printf("Enter Number 1: "); 
+        scanf("%d", &num1);
+
+        printf("Enter Number 1: "); 
+        scanf("%d", &num1);
+
+        // Results Before Swapping    
+        printf("Before Swapping\nnum1 : %d\nnum2 : %d\n", num1, num2); 
+        // Using Function Swap and giving the position of the two variables
+        Swap(&num1, &num2);
+        // Results After Swapping    
+        printf("After Swapping\nnum1 : %d\nnum2 : %d\n", num1, num2); 
+        
+
+    }
