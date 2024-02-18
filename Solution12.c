ce1164081b73b1007d41bb2f0822992e367fc8cd
socketgut <71415224+varadpal@users.noreply.github.com>
2024-02-18 09:38:16 +0530

    Correct Solution12.c

--- Solution12.c
+++ Solution12.c
@@ -1,1 +1,17 @@
+// Find sum of all matrix elements using pointer.
 
+#include <stdio.h>
+
+int main(int argc, char const *argv[]){
+    int mat[3][3] = {{1,2,3}, {4,5,6}, {7,8,9}}; //45
+    int result = 0; 
+
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            result += *(*(mat + i) + j);    
+        }
+    }
+    
+    printf("Sum of All Elements = %d", result);
+    return 0;
+}
