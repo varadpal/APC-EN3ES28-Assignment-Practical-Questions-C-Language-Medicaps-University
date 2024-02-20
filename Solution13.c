e7fc7cabc34290abb2480356ec09a5e9fc46b240
socketgut <71415224+varadpal@users.noreply.github.com>
2024-02-20 19:43:04 +0530

    Update Solution13.c

--- Solution13.c
+++ Solution13.c
@@ -2,22 +2,24 @@
 #include <stdio.h>
 
 int main() {
-    int range; 
-    printf("Enter Range of Array: "); scanf("%d", &range);
+    // int range; 
+    // printf("Enter Range of Array: "); 
+    // scanf("%d", &range);
     
-    int arr[range] = {}; 
+    int arr[5] = {}; 
 
     int *ptr;
     ptr = arr;
 
     // For Taking Input
-    for(int i =0; i < range; i++) {
+    for(int i = 0; i < 5; i++) {
         printf("Enter Value %d: ", i+1);
-        scanf("%d", *(ptr+i)); // *ptr[i]
+        scanf("%d", &(*(ptr+i)));
     }
 
     // For Printing Output
-    for(int j = 0; j < range; j++){
-        printf("%d", *(ptr +j));
+    printf("The Array Entered:\n");
+    for(int j = 0; j < 5; j++){
+        printf("%d\n", *(ptr+j));
     }
 }
